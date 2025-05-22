31fd726ebff414bd0c2358c72b7199514acd6a09
Susan Lutz <suelutz@gmail.com>
2025-05-22 16:41:03 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -25,33 +25,6 @@ function myFunctionDarkMode() {
   navbar.classList.toggle("dark-mode");
 }
 
-/*function changeSizeByBtn(size) {
-    var elements = document.getElementsByClassName("fontSizeClass");
-    for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var origFontSize = Number(element.style.fontSize);
-    origFontSize = origFontSize * size;
-    console.log(element.style.fontSize);}
-  }
-
-  function changeSizeByBtn(size) {
-    var allTextContent = document.body.textContent;
-    /*var allTextContent = document.getElementsByClassName("fontSizeClass");
-    for (var i = 0; i < allTextContent.length; i++) {
-    var element = allTextContent[i];
-    var origFontSize = Number(element.style.fontSize);
-    origFontSize = origFontSize * size;
-    console.log(element.style.fontSize);}
-  }
-
-  function changeFontSize(multiplier) {
-    var div = document.getElementById("wholePage");
-    var currentFont = div.style.fontSize.replace("px", "");
-    div.style.fontSize = parseInt(currentFont) * parseInt(multiplier) + "px";
-}
-
-*/
-
 var cont = document.getElementById("wholePage");
 function changeSizeByBtn(multiplier) {
     // Set value of the parameter as fontSize
@@ -83,12 +56,11 @@ window.addEventListener('load', function(e) {
   }
   });
 
-
  let cont = document.getElementById("container");
 function changeSizeByBtn(size) {
     // Set value of the parameter as fontSize
     cont.style.fontSize = size;
-}*/
+}
 
 function changeSizeByBtn(size) {
   var elements = document.getElementsByClassName("fontSizeClass");
