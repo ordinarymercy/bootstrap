4717aada5d25c06d14e82655fa73ce4e207429df
Susan Lutz <suelutz@gmail.com>
2025-05-25 09:41:57 -0500

    commit

--- code.js
+++ code.js
@@ -8,3 +8,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+/*from faq.html... */
+
+  function toggleDetail(e) {
+    const title = e.currentTarget;
+    const detail = title.nextElementSibling;
+    toggleDisplay(detail);
+    const chevron = title.querySelector('.chevron');
+    chevron.classList.toggle('fa-chevron-down');
+    chevron.classList.toggle('fa-chevron-up');
+  }
+
+  window.addEventListener('load', function(e) {
+    const titles = document.querySelectorAll('.title');
+    for (title of titles) {
+      title.addEventListener('click', toggleDetail);
+    } 
+    }
+    
+  );
+
